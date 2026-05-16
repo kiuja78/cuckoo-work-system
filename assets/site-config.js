3526851efb95d01e6aef9c8c7ce21f43c5581500
kiuja78 <kiuja78@gmail.com>
2026-05-17 00:02:54 +0900

    Update site-config.js

--- assets/site-config.js
+++ assets/site-config.js
@@ -1,7 +1,7 @@
 // 사이트 기본 설정 파일입니다.
 // GitHub 저장소 이름을 바꾸면 아래 값을 수정하세요.
 window.SITE_CONFIG = {
-  latestVersion: "v1.0.0",
+  latestVersion: "업무자동화시스템v1.10.48",
   latestDate: "2026.05.16",
   githubOwner: "kiuja78",
   githubRepo: "cuckoo-work-system",
